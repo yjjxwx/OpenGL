44ea3b0397ed26e0a8a71b40f2957879412ecd44
klliu <klliu@lewatek.com>
2014-08-31 13:48:12 +0800

    Title: Implements draw two triangle on screen.

--- triangle/main.cpp
+++ triangle/main.cpp
@@ -17,10 +17,15 @@ void init(){
 }
 void display(){
     glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_POINTS);
-    glVertex2i(100,50);
-    glVertex2i(100,130);
-    glVertex2i(150,130);
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.0f,0.0f,0.0f);
+    glVertex2i(2,2);
+    glVertex2i(510,2);
+    glVertex2i(2,510);
+    glColor3f(0.0f,1.0f,0.0f);
+    glVertex2i(510,2);
+    glVertex2i(510,510);
+    glVertex2i(2,510);
     glEnd();
     glFlush();
 }
